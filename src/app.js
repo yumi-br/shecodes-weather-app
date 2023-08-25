c4a67d8de6fcd195901e0dea158cb733643c6ea5
yumi-br <132803304+yumi-br@users.noreply.github.com>
2023-08-25 11:39:00 -0300

    deleted alt from img

--- src/app.js
+++ src/app.js
@@ -86,4 +86,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("New York");
+search("Florianopolis");
